94ad79bdd7ddf23ce6a5a72e8c7cadd4d0aefadf
uncov-projects <uncovedu@gmail.com>
2025-03-22 22:21:49 +0530

    Added files to src
    1. index.css
    2. App.css
    3. App.jsx
    4. main.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SliderAuth from "./components/SliderAuth";
+
+function App() {
+  return (
+    <div className="App">
+      <SliderAuth />
+    </div>
+  );
+}
+
+export default App;
